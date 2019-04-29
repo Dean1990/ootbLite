19ab166af167444933565f485e5ea692ec766bf4
Dean <abcruoshui@sina.com>
2019-04-29 18:23:26 +0800

    coding

--- app/src/main/java/com/deanlib/ootblite/utils/AppUtils.java
+++ app/src/main/java/com/deanlib/ootblite/utils/AppUtils.java
@@ -1,6 +1,7 @@
 package com.deanlib.ootblite.utils;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
@@ -158,6 +159,30 @@ public class AppUtils {
             //创建分享的Dialog
             share_intent = Intent.createChooser(share_intent, dialogTitle);
             activity.startActivity(share_intent);
+        }
+    }
+
+    /**
+     * 跳转应用商店.
+     *
+     * @param context   {@link Context}
+     * @param appPkg    包名
+     * @param marketPkg 应用商店包名
+     * @return {@code true} 跳转成功 <br> {@code false} 跳转失败
+     */
+    public static boolean openMarket(Context context, String appPkg, String marketPkg) {
+        Uri uri = Uri.parse("market://details?id=" + appPkg);
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        if (marketPkg != null) {// 如果没给市场的包名，则系统会弹出市场的列表让你进行选择。
+            intent.setPackage(marketPkg);
+        }
+        try {
+            context.startActivity(intent);
+            return true;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return false;
         }
     }
 
